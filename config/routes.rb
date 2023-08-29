8a904b8c49a40a65610c52e5c535a37dedd98c7d
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-08-29 21:27:23 +0200

    Modify the routes according to the requirements

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions', # Customize sessions controller for login
+    registrations: 'users/registrations' # Customize registrations controller for signup
+  }
   resources :shots
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'shots#index'
