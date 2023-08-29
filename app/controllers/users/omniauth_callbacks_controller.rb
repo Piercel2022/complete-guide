d185c44146311e8fb01925500e16498fd549ad79
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-08-30 00:58:01 +0200

    Add the worklfow

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,30 +1,36 @@
 # frozen_string_literal: true
 
-class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  # You should configure your model like this:
-  # devise :omniauthable, omniauth_providers: [:twitter]
+class Devise::OmniauthCallbacksController < DeviseController
+  prepend_before_action { request.env["devise.skip_timeout"] = true }
 
-  # You should also create an action method in this controller like this:
-  # def twitter
-  # end
+  def passthru
+    render status: 404, plain: "Not found. Authentication passthru."
+  end
 
-  # More info at:
-  # https://github.com/heartcombo/devise#omniauth
+  def failure
+    set_flash_message! :alert, :failure, kind: OmniAuth::Utils.camelize(failed_strategy.name), reason: failure_message
+    redirect_to after_omniauth_failure_path_for(resource_name)
+  end
 
-  # GET|POST /resource/auth/twitter
-  # def passthru
-  #   super
-  # end
+  protected
 
-  # GET|POST /users/auth/twitter/callback
-  # def failure
-  #   super
-  # end
+  def failed_strategy
+    request.respond_to?(:get_header) ? request.get_header("omniauth.error.strategy") : request.env["omniauth.error.strategy"]
+  end
 
-  # protected
+  def failure_message
+    exception = request.respond_to?(:get_header) ? request.get_header("omniauth.error") : request.env["omniauth.error"]
+    error   = exception.error_reason if exception.respond_to?(:error_reason)
+    error ||= exception.error        if exception.respond_to?(:error)
+    error ||= (request.respond_to?(:get_header) ? request.get_header("omniauth.error.type") : request.env["omniauth.error.type"]).to_s
+    error.to_s.humanize if error
+  end
 
-  # The path used when OmniAuth fails
-  # def after_omniauth_failure_path_for(scope)
-  #   super(scope)
-  # end
+  def after_omniauth_failure_path_for(scope)
+    new_session_path(scope)
+  end
+
+  def translation_scope
+    'devise.omniauth_callbacks'
+  end
 end
