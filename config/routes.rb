e2af1d8f93d4fbe8666db5161e31149201ac6a74
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-08-30 09:55:16 +0200

    Add the worklfow

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+
+  root 'shots#index'
   devise_for :users
   
   resources :shots
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   #root 'shots#index'
-  root 'shots#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
